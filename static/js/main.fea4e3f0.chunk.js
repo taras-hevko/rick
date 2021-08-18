527e60cecd214c16a17cba4a74dc603af5d68a1f
Taras Hevko <hevko.tars@gmail.com>
2021-08-18 12:35:43 +0300

    Updates

---
+++ static/js/main.fea4e3f0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprick=this.webpackJsonprick||[]).push([[0],{9:function(e,c,t){"use strict";t.r(c);var n=t(1),r=t.n(n),s=t(3),i=t.n(s),j=t(0),p=[1,2,3,4,5,6];function d(){return Object(j.jsxs)("div",{className:"App",children:[Object(j.jsx)("h1",{children:"Me app"}),p.map((function(e,c){return Object(j.jsx)("p",{children:e},p[c+1])})),Object(j.jsx)("p",{children:"asd"})]})}i.a.render(Object(j.jsx)(r.a.StrictMode,{children:Object(j.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.fea4e3f0.chunk.js.map
